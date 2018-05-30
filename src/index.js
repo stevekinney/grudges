99892b9cb47ad23b89e3d1be8374135122be2c88
Steve Kinney <hello@stevekinney.net>
2018-05-30 13:41:10 -0500

    Wire in basic AWS Mobile Hub settings

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Application from './Application';
 
+import Amplify from 'aws-amplify';
+import configuration from './aws-exports';
+
+Amplify.configure(configuration);
+
 ReactDOM.render(<Application />, document.getElementById('root'));
