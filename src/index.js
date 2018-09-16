67cff1bdec68a8751f870d16d265dfd20b485d1a
Panda Geeks <pandageeks@lavies-macbook.attlocal.net>
2018-09-15 19:38:27 -0500

    wire up basic aws mobile hub settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Application from './Application';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Application from "./Application";
 
-ReactDOM.render(<Application />, document.getElementById('root'));
+// link from backend to frontend
+import Amplify from "aws-amplify";
+import configuration from "./aws-exports";
+
+Amplify.configure(configuration);
+
+ReactDOM.render(<Application />, document.getElementById("root"));
